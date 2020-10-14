e2b7d170667f912bcf0e0b030ebdbf03932640dc
vanessabach <vanessabach.r@gmail.com>
2020-10-13 22:14:58 -0300

    missing views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+puts "Creating restaurants..."
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+  puts "Now creating the reviews"
+  5.times do
+    review = Review.new(
+      rating: (0..5).to_a.sample,
+      content: Faker::Quote.famous_last_words,
+      restaurant_id: Restaurant.maximum('id')
+    )
+    review.save!
+  end
+end
+puts 'Finished!'
